b7d8f1606ef32674ed2c8f1f09d13c5c488a9acc
rp <rp@iclicks.io>
2024-05-02 12:26:51 +0200

    убрал редиркет

--- IclicksYandexCloud/obj/Debug/net8.0/IclicksYandexCloud.AssemblyInfo.cs
+++ IclicksYandexCloud/obj/Debug/net8.0/IclicksYandexCloud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IclicksYandexCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa536bfd1d3b89e7a13cf49521b67c15485d9c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ff58dd5e41f21c34d255055abcde13b26172ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("IclicksYandexCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IclicksYandexCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
